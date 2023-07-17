ceb8793dffc6d0af92735f07c71407047c9e5cf9
wizd <rcvbuf@gmail.com>
2023-07-17 08:39:09 +0800

    make it a pure proxy

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // Server configuration
 export const SERVER_PORT = 3000; // Server port
-export const DEBUG = false; // Debug mode
+export const DEBUG = true; // Debug mode
 
 // Prompt Moderation before sending to OpenAI
-export const MODERATION = true; // Moderation mode
+export const MODERATION = false; // Moderation mode
 
 // Rate limit
 export const PRIOD = 15 * 1000; // 15 seconds
@@ -11,13 +11,13 @@ export const RATE_LIMIT = 50; // 50 requests per 15 seconds
 
 // Whitelisted IPs
 export const WHITELISTED_IPS = [
-    // "127.0.0.1"
+  // "127.0.0.1"
 ];
 
 // OpenAI API Keys
 export let OPENAI_KEYS = [
-    "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 ];
